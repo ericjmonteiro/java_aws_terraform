e56221788dcbdb471b8c99b0e5cdf68c5dc7bf8d
Eric Monteiro <ericjmonteiro@gmail.com>
2023-03-12 23:21:16 -0300

    add docker compose

--- src/main/java/br/com/xerosorvetes/flavorapi/FlavorApiApplication.java
+++ src/main/java/br/com/xerosorvetes/flavorapi/FlavorApiApplication.java
@@ -2,12 +2,14 @@ package br.com.xerosorvetes.flavorapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class FlavorApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlavorApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FlavorApiApplication.class, args);
+  }
 
 }
