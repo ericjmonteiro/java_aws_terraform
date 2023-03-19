642520592b0c115f83d6dc6000f4c04d8f8052ad
Eric Monteiro <ericjmonteiro@gmail.com>
2023-03-19 19:28:52 -0300

    add terraform configuration

--- terraform/security.tf
+++ terraform/security.tf
@@ -2,6 +2,10 @@ resource "aws_security_group" "flavor_sg" {
   name        = "flavorapi_sg"
   description = "flavor api security group"
   vpc_id      = aws_vpc.flavor_vpc_1.id
+
+  tags = {
+    "Name" = "flavorapi_sg"
+  }
 }
 
 resource "aws_security_group_rule" "sg_public_out" {
@@ -22,16 +26,25 @@ resource "aws_security_group_rule" "sg_ssh_in" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "sg_https_in" {
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.flavor_sg.id
-  to_port           = 443
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
 resource "aws_key_pair" "flavor_key" {
   public_key = file("~/.ssh/flavor_api.pub")
   key_name   = "flavor_api"
+}
+
+resource "aws_security_group_rule" "public_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.flavor_sg.id
+}
+
+resource "aws_security_group_rule" "public_in_http_83" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 8083
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.flavor_sg.id
 }
